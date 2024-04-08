6844bfbcb1cf6d2216b7161f1a9d6664a19cfff9
TAQI TAHMID <140246524+taqiTahmid1999@users.noreply.github.com>
2024-04-08 21:24:30 +0600

    Create loop.js

---
+++ js/loop.js
@@ -0,0 +1,27 @@
+//Array(value,index)
+//Object(property name, property value)
+// Set: Collection of unique values
+// Syntax: new Set([iterable])
+// Value, Index (not applicable in sets)
+// Map: Collection of key-value pairs
+// Syntax: new Map([iterable])
+// Key, Value
+
+
+//forEach Loop
+/*ArrayName/ObjectName/SetName/MapName.forEach(functionName);
+function functionName(value(array/set)/property value(object)/value(map), index(array/set)/property name(object)/key(map), array/set/object/map){
+  //code
+  //does not return anything
+}
+
+//for in loop
+let x;//index number of array/set or property name of object
+for( x in ObjectName/ArrayName/setName ){
+  //code
+}
+//for of loop
+let x;//value of every index of array/set or property value of object or pairs([key,value]) of map
+for(let x of ArrayName/SetName/ObjectName/MapName.entries()){
+  //code
+}
